3182ac4b0896660a065934652a98a184a1441e5f
Yaroslav Stavnichiy <yarosla@gmail.com>
2016-11-07 00:35:03 +0300

    pages

--- src/app.bootstrap.ts
+++
@@ -1,6 +0,0 @@
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
-import {AppModule} from './app.module';
-import './index.scss';
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-    .catch(err => console.error(err));
--- src/globals.d.ts
+++
@@ -1,7 +0,0 @@
-declare module '*.html' {
-    const _: string;
-    export = _;
-}
-
-// fix angular2-template-loader error: Cannot find name 'require':
-declare function require(string: string): any;
